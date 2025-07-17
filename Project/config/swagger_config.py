bf14249cdf0ce64f38bca159f4a96dbc880604c7
AlexCazan <62256824+AlexCazan@users.noreply.github.com>
2025-07-17 16:25:20 +0300

    Added Swagger

---
+++ Project/config/swagger_config.py
@@ -0,0 +1,16 @@
+from fastapi.openapi.utils import get_openapi
+
+def swagger_openapi(app):
+    def custom_openapi():
+        if app.openapi_schema:
+            return app.openapi_schema
+        openapi_schema = get_openapi(
+            title="HomeworkPython API",
+            version="1.0.0",
+            description="This is a custom OpenAPI",
+            routes=app.routes,
+        )
+        app.openapi_schema = openapi_schema
+        return app.openapi_schema
+
+    return custom_openapi
--- Project/main.py
+++ Project/main.py
@@ -1,2 +1,14 @@
-print("ceva")
-print('Im just a girl')
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+from config.swagger_config import swagger_openapi
+from api.math_router import router as math_router   
+
+app = FastAPI()
+app.openapi = swagger_openapi(app)
+
+# Redirect root to Swagger UI
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url="/docs")
+
+app.include_router(math_router)
